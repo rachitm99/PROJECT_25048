14ac3ec498874412b6182dd8f11acaef7b16d070
rachitm99 <rachitmukherjee99@gmail.com>
2022-04-02 15:54:17 +0530

    first look

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['media0.giphy.com','file-examples.com','localhost']
+},
+
 }
 
 module.exports = nextConfig
