13749d6c437d689e43532146c6f176a4be46564d
rachitm99 <rachitmukherjee99@gmail.com>
2022-04-04 09:57:25 +0530

    kind of implemented the mobile view but still require some fine edge finishing and still need to implement rotating baaner and seperate images for mobile view

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['media0.giphy.com','file-examples.com','localhost']
+    domains: ['media0.giphy.com','file-examples.com','localhost','aumnix.com']
 },
 
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      object:{
+        'photo':'right 75%'
+      }
+    },
   },
   plugins: [],
 }
